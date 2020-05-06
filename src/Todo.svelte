b3adca1bd023f981b213518069e538edbb9700df
Caio Teixeira <caiofct@gmail.com>
2020-05-05 22:49:20 -0300

    First Commit

---
+++ src/Todo.svelte
@@ -0,0 +1,23 @@
+<script>
+  let todos = ['Task 1', 'Task 2', 'Task 3']
+
+  function addTask() {
+    todos = [...todos, 'New Task']
+  }
+</script>
+
+<main>
+  <ul>
+    {#each todos as todo}
+      <li>{todo}</li>
+    {/each}
+  </ul>
+
+  <button on:click={addTask}>Add Task</button>
+</main>
+
+<style>
+  ul {
+    list-style-type: none;
+  }
+</style>
